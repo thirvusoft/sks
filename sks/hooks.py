6db85e1e2d0580ae5521f5e3c6005d335c825a9e
Gokulnath17 <gokulnath@thirvusoft.in>
2022-03-15 13:38:12 +0530

    checking sales order items and delivery note items are same or not

--- sks/hooks.py
+++ sks/hooks.py
@@ -32,6 +32,8 @@ app_license = "MIT"
 
 # include js in doctype views
 # doctype_js = {"doctype" : "public/js/doctype.js"}
+doctype_js = {"Delivery Note" : "sks/custom/js/delivery_note_test_with_sales_order.js"}
+
 # doctype_list_js = {"doctype" : "public/js/doctype_list.js"}
 # doctype_tree_js = {"doctype" : "public/js/doctype_tree.js"}
 # doctype_calendar_js = {"doctype" : "public/js/doctype_calendar.js"}
