931474fb7312170fd77e46d9f63c2ee3c2329ef5
gokulnath17 <gokulnath@thirvusoft.in>
2022-03-23 15:28:29 +0530

    Reserved Stock alert in sales order,sales invoice and delivery note

--- sks/sks/custom/js/sales_order.js
+++ sks/sks/custom/js/sales_order.js
@@ -48,12 +48,11 @@ frappe.ui.form.on("Sales Order",{
                     })
                 }
             }
-        })    
+        })   
     }
  })
- 
-
-frappe.ui.form.on("Sales Order",{
+  
+ frappe.ui.form.on("Sales Order",{
     set_warehouse:function(frm,cdt,cdn){
         var data=locals[cdt][cdn]
         frappe.call({
@@ -71,35 +70,8 @@ frappe.ui.form.on("Sales Order",{
             }
         })
     }
-})
+ })
 
-
-frappe.ui.form.on("Sales Order",{
-	after_save:function(frm,cdt,cdn){
-        frappe.db.get_single_value("SKS Settings","reserved_stock").then(value =>{
-            if(value==1){
-                var status=cur_frm.doc.docstatus
-                if(status==1){
-                    var data=locals[cdt][cdn]
-                    var item_codes=[]
-                    var source_warehouse=[]
-                    var required_qty=[]
-                    var basic_rate=[]
-                    for(var i=0;i<data.items.length;i++){
-                        item_codes.push(data.items[i].item_code)
-                        source_warehouse.push(data.items[i].warehouse)
-                        required_qty.push(data.items[i].qty)
-                        basic_rate.push(data.items[i].amount)
-                    }
-                    frappe.call({
-                        method:"sks.sks.custom.py.sales_order.reserved_stock_for_sales_order",
-                        args:{item_codes,source_warehouse,required_qty,basic_rate}
-                    })
-                }
-            }
-        })
-	}
-})
 
 var loop
 frappe.ui.form.on("Sales Order",{
@@ -109,45 +81,87 @@ frappe.ui.form.on("Sales Order",{
 })
 frappe.ui.form.on("Sales Order",{
 	after_save:function(frm,cdt,cdn){
-        if(cur_frm.doc.docstatus!=1){
-            if(loop==0){
-                var data1 = locals[cdt][cdn]
-                var customer = data1.customer
-                frappe.call({
-                    method:"sks.sks.custom.py.sales_order.customer_credit_sale",
-                    args:{customer},
-                    callback : function(r){
-                        if(r.message[2]>0){
-                            var d = new frappe.ui.Dialog({
-                                size: "extra large",
-                                title:"Customer: "+ customer +"'s Outstanding Amount",
-                                fields:[
-                                    {'fieldname':'alert','fieldtype':'HTML','read_only':1,'bold':1},
-                                    {'label':'Outstanding Amount','fieldname':'outstanding','fieldtype':'Currency','default':r.message[2],'read_only':1},
-                                ],
-                                primary_action : function(data){
-                                    loop=loop+1
-                                    frm.set_value("outstanding_amount",data.outstanding)
-                                    var data1 = locals[cdt][cdn]
-                                    var over_all_total = data1.outstanding_amount+data1.total
-                                    cur_frm.set_value("outstanding_amount_and_total",over_all_total)
-                                    d.hide();
+        frappe.db.get_single_value("SKS Settings","credit_bill_history").then(value =>{
+            if(value==1){
+                if(cur_frm.doc.docstatus!=1){
+                    if(loop==0){
+                        var data1 = locals[cdt][cdn]
+                        var customer = data1.customer
+                        frappe.call({
+                            method:"sks.sks.custom.py.sales_order.customer_credit_sale",
+                            args:{customer},
+                            callback : function(r){
+                                if(r.message[2]>0){
+                                    var d = new frappe.ui.Dialog({
+                                        size: "extra large",
+                                        title:"Customer: "+ customer +"'s Outstanding Amount",
+                                        fields:[
+                                            {'fieldname':'alert','fieldtype':'HTML','read_only':1,'bold':1},
+                                            {'label':'Outstanding Amount','fieldname':'outstanding','fieldtype':'Currency','default':r.message[2],'read_only':1},
+                                        ],
+                                        primary_action : function(data){
+                                            loop=loop+1
+                                            frm.set_value("outstanding_amount",data.outstanding)
+                                            var data1 = locals[cdt][cdn]
+                                            var over_all_total = data1.outstanding_amount+data1.total
+                                            cur_frm.set_value("outstanding_amount_and_total_amount",over_all_total)
+                                            d.hide();
 
+                                        }
+                                    })
+                                    var template=r.message[3]
+                                    d.set_df_property("alert","options",frappe.render(template,{}))
+                                    d.show();
                                 }
-                            })
-                            var template=r.message[3]
-                            d.set_df_property("alert","options",frappe.render(template,{}))
-                            d.show();
-                        }
-                    }  
-                })
+                            }  
+                        })
+                    }
+                }
+                if(loop==0){
+                    loop=loop+1
+                    var data1 = locals[cdt][cdn]
+                    var over_all_total = data1.outstanding_amount+data1.total
+                    cur_frm.set_value("outstanding_amount_and_total_amount",over_all_total)
+                }
             }
-        }
-        if(loop==0){
-            loop=loop+1
-            var data1 = locals[cdt][cdn]
-            var over_all_total = data1.outstanding_amount+data1.total
-            cur_frm.set_value("outstanding_amount_and_total",over_all_total)
-        }
+        })
     }
 })
+  
+ var parent_data
+ frappe.ui.form.on("Sales Order",{
+    onload:function(frm,cdt,cdn){
+        parent_data=locals[cdt][cdn]
+    }
+ })
+ frappe.ui.form.on("Sales Order Item",{
+    qty:function(frm,cdt,cdn){
+        frappe.db.get_single_value("SKS Settings","reserved_stock").then(value =>{
+            if(value==1){
+                var data = locals[cdt][cdn]
+                var item_code=data.item_code
+                var item_qty=data.qty
+                var source_warehouse=data.warehouse
+                frappe.call({
+                    method:"erpnext.stock.dashboard.item_dashboard.get_data",
+                    args:{item_code,warehouse:source_warehouse},
+                    callback(r){
+                        var projected_qty=r.message[0].projected_qty    
+                        if(item_qty>projected_qty){
+                            for(var i=0;i<parent_data.items.length;i++){
+                                if(item_code==parent_data.items[i].item_code){
+                                    frappe.model.set_value(parent_data.items[i].doctype,parent_data.items[i].name,"qty",0)
+                                    frappe.throw({
+                                        title:"Stock Unavailable",
+                                        message:"For Item : "+item_code+", The Quantity must be less than : "+projected_qty
+                                    })
+                                }
+                            }
+                        }
+                    }
+                })
+            }
+        })
+    }
+ })
+ 
