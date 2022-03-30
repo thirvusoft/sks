33d172da122be1c52acac10654ebd7ad2d7e8f3a
gokulnath17 <gokulnath@thirvusoft.in>
2022-03-30 14:14:06 +0530

    Item verified bugs fixes in Sales Invoice

--- sks/sks/custom/js/sales_invoice.js
+++ sks/sks/custom/js/sales_invoice.js
@@ -5,56 +5,62 @@ frappe.db.get_single_value("SKS Settings","allow_only_if_sales_invoice_items_mat
 	frappe.ui.form.on("Sales Invoice",{
 		scan_barcode_to_verify_the_items: function(frm,cdt,cdn){
 			let data=locals[cdt][cdn]
-			var search_value = data.scan_barcode_to_verify_the_items
-			var checking_sales_invoice = data.items[0].sales_order
-			if(search_value !="")
-			{
-			frappe.call({
-				method:"erpnext.selling.page.point_of_sale.point_of_sale.search_for_serial_or_batch_or_barcode_number",
-				args:{search_value},
-				callback(r){
-					var item_code_checking = r["message"]["item_code"]
+			// console.log(data.)
+			if(data.update_stock==1){
+				var search_value = data.scan_barcode_to_verify_the_items
+				var checking_sales_invoice = data.items[0].sales_order
+				if(search_value !="")
+				{
 					frappe.call({
-						 method:"sks.sks.custom.py.sales_invoice.item_check_with_sales_order",
-						 args:{item_code_checking,checking_sales_invoice},
-						 callback(r){
-							if(r["message"]==0){
-								frappe.msgprint("Scanned Barcode Is Not Matching With The Sales Order Items")  
-							}
-							else{
-								for(var i=0;i<data.items.length;i++){
-									if(r["message"]==data.items[i].item_code){
-										frappe.model.set_value(data.items[i].doctype,data.items[i].name,"item_verified",1)
+						method:"erpnext.selling.page.point_of_sale.point_of_sale.search_for_serial_or_batch_or_barcode_number",
+						args:{search_value},
+						callback(r){
+							var item_code_checking = r["message"]["item_code"]
+							frappe.call({
+								method:"sks.sks.custom.py.sales_invoice.item_check_with_sales_order",
+								args:{item_code_checking,checking_sales_invoice},
+								callback(r){
+									if(r["message"]==0){
+										frappe.msgprint("Scanned Barcode Is Not Matching With The Sales Order Items")  
+									}
+									else{
+										for(var i=0;i<data.items.length;i++){
+											if(r["message"]==data.items[i].item_code){
+												frappe.model.set_value(data.items[i].doctype,data.items[i].name,"item_verified",1)
+											}
+										}
+										frappe.show_alert({ message: __('Item Matched'), indicator: 'green' });
+										frappe.model.set_value(cdt,cdn,"scan_barcode_to_verify_the_items","")
 									}
 								}
-								frappe.show_alert({ message: __('Item Matched'), indicator: 'green' });
-								frappe.model.set_value(cdt,cdn,"scan_barcode_to_verify_the_items","")
-							}
+							})
 						}
 					})
 				}
-			})
 			}
 		}
 	})
 	frappe.ui.form.on("Sales Invoice",{
 		before_save: function(frm,cdt,cdn){
-			var total_matched_items=0
-			var not_verified_items=[]
-			let data=locals[cdt][cdn]
-			for(var i=0;i<data.items.length;i++){
-				if(data.items[i].item_verified==0){
-					not_verified_items=not_verified_items+data.items[i].item_name
-					if(data.items.length != i+1){
-						not_verified_items=not_verified_items+", "
+			var data=locals[cdt][cdn]
+			if(data.update_stock==1){
+				var total_matched_items=0
+				var not_verified_items=[]
+				let data=locals[cdt][cdn]
+				for(var i=0;i<data.items.length;i++){
+					if(data.items[i].item_verified==0){
+						not_verified_items=not_verified_items+data.items[i].item_name
+						if(data.items.length != i+1){
+							not_verified_items=not_verified_items+", "
+						}
+					}
+					else{
+						total_matched_items=total_matched_items+1
 					}
 				}
-				else{
-					total_matched_items=total_matched_items+1
+				if(total_matched_items!=data.items.length){
+					frappe.throw(not_verified_items+" are not verified, please check it...")
 				}
-			}
-			if(total_matched_items!=data.items.length){
-				frappe.throw(not_verified_items+" are not verified, please check it...")
 			}
 		}
 	})
