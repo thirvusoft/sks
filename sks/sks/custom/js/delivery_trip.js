36cd2f8b8e3fe65da21b8a2bd4fd195d67702ee1
SaraneshThirvu <saranesh@thirvusoft.in>
2022-04-21 14:52:54 +0530

    changes

--- sks/sks/custom/js/delivery_trip.js
+++ sks/sks/custom/js/delivery_trip.js
@@ -2,14 +2,95 @@ frappe.ui.form.on('Delivery Trip',{
     territory : function(frm,cdt,cdn){
         let p = locals[cdt][cdn]
         if(p.territory){
+            if(p.delivery_date){
+                frappe.call({
+                    method : "sks.sks.custom.py.delivery_trip.get_sales_invoice",
+                    args : {territory : p.territory, date : p.delivery_date},
+                    callback(r){
+                        console.log(r.message)
+                        frm.set_value("delivery_stops",[])
+                            for(let i = 0; i<r.message.length;i++){
+                            cur_frm.add_child("delivery_stops")
+                            frappe.model.set_value(p.delivery_stops[i].doctype,p.delivery_stops[i].name,"customer",r.message[i]["customer"])
+                            frappe.model.set_value(p.delivery_stops[i].doctype,p.delivery_stops[i].name,"address",r.message[i]["customer_address"])
+                            frappe.model.set_value(p.delivery_stops[i].doctype,p.delivery_stops[i].name,"sales_invoice",r.message[i]["name"])
+                            frappe.model.set_value(p.delivery_stops[i].doctype,p.delivery_stops[i].name,"delivery_status",r.message[i]["delivery_status"])
+                            frappe.model.set_value(p.delivery_stops[i].doctype,p.delivery_stops[i].name,"reason",r.message[i]["reason"])
+                            console.log("aaaaaaaaaaaaaaaaaaa")
+                        }
+                        cur_frm.refresh_field('delivery_stops')
+                    }
+                })
+            }
+        }
+    },
+    delivery_date: function(frm,cdt,cdn){
+        let p = locals[cdt][cdn]
+        if(p.territory){
+            if(p.delivery_date){
+                frappe.call({
+                    method : "sks.sks.custom.py.delivery_trip.get_sales_invoice",
+                    args : {territory : p.territory, date : p.delivery_date},
+                    callback(r){
+                        console.log(r.message)
+                        frm.set_value("delivery_stops",[])
+                            for(let i = 0; i<r.message.length;i++){
+                            cur_frm.add_child("delivery_stops")
+                            frappe.model.set_value(p.delivery_stops[i].doctype,p.delivery_stops[i].name,"customer",r.message[i]["customer"])
+                            frappe.model.set_value(p.delivery_stops[i].doctype,p.delivery_stops[i].name,"address",r.message[i]["customer_address"])
+                            frappe.model.set_value(p.delivery_stops[i].doctype,p.delivery_stops[i].name,"sales_invoice",r.message[i]["name"])
+                            frappe.model.set_value(p.delivery_stops[i].doctype,p.delivery_stops[i].name,"delivery_status",r.message[i]["delivery_status"])
+                            frappe.model.set_value(p.delivery_stops[i].doctype,p.delivery_stops[i].name,"reason",r.message[i]["reason"])
+                            console.log("aaaaaaaaaaaaaaaaaaa")
+                        }
+                        cur_frm.refresh_field('delivery_stops')
+                    }
+                })
+            }
+        }
+    },
+    update_invoice: function(frm,cdt,cdn){
+        if(frm.doc.sales_invoice){
             frappe.call({
-                method : "sks.sks.custom.py.delivery_trip.get_sales_invoice",
-                args : {territory : p.territory},
-                callback(r){
-                    console.log(r.message)
-                    frappe.model.set_value(cdt,cdn,"delivery_stops",[])
-                    frappe.model.set_value(cdt,cdn,"delivery_stops",r.message)
-                }
+                method: "sks.sks.custom.py.delivery_trip.update_invoice",
+                args:{
+                    invoice: frm.doc.sales_invoice,
+                    fields:{
+                        delivery_status: frm.doc.delivery_status,
+                        reason:frm.doc.reason || ""
+                    },
+                    callback(res){
+                        alert(frm.doc.reason)
+                        console.log("Success")
+                        let p = locals[cdt][cdn]
+                        if(p.territory){
+                            if(p.delivery_date){
+                                for(let i=0;i<2;i++){
+                                frappe.call({
+                                    method : "sks.sks.custom.py.delivery_trip.get_sales_invoice",
+                                    args : {territory : p.territory, date : p.delivery_date},
+                                    callback(r){
+                                        console.log(r.message)
+                                        frm.set_value("delivery_stops",[])
+                                            for(let i = 0; i<r.message.length;i++){
+                                            cur_frm.add_child("delivery_stops")
+                                            frappe.model.set_value(p.delivery_stops[i].doctype,p.delivery_stops[i].name,"customer",r.message[i]["customer"])
+                                            frappe.model.set_value(p.delivery_stops[i].doctype,p.delivery_stops[i].name,"address",r.message[i]["customer_address"])
+                                            frappe.model.set_value(p.delivery_stops[i].doctype,p.delivery_stops[i].name,"sales_invoice",r.message[i]["name"])
+                                            frappe.model.set_value(p.delivery_stops[i].doctype,p.delivery_stops[i].name,"delivery_status",r.message[i]["delivery_status"])
+                                            frappe.model.set_value(p.delivery_stops[i].doctype,p.delivery_stops[i].name,"reason",r.message[i]["reason"])
+                                            cur_frm.refresh_field('delivery_stops')
+                                            console.log("aaaaaaaaaaaaaaaaaaa")
+                                        }
+                                        cur_frm.refresh_field('delivery_stops')
+                                        frm.refresh()
+                                    }
+                                })
+                            }
+                            }
+                        }
+                    }
+                } 
             })
         }
     }
--- sks/sks/custom/py/delivery_trip.py
+++ sks/sks/custom/py/delivery_trip.py
@@ -1,8 +1,23 @@
 import frappe
+import json
 @frappe.whitelist()
-def get_sales_invoice(territory):
-    sales =  frappe.db.sql('''
-    select name as sales_invoice, customer_address as address, customer, delivery_status from `tabSales Invoice` where territory = "{0}"
-    '''.format(territory),as_dict = 1)
+def get_sales_invoice(territory,date):
+    salesorder = frappe.get_all("Sales Order",filters = {"delivery_date":date},pluck = 'name')
+    frappe.errprint(salesorder)
+    sales_invoice = frappe.get_all("Sales Invoice Item",filters = {"sales_order":["in",salesorder]},pluck = 'parent')
+    frappe.errprint(sales_invoice)
+    sales = frappe.get_all("Sales Invoice",fields=['name','customer_address','customer','delivery_status','reason'],filters ={'territory':territory,'name':['in',sales_invoice]})
+    # sales =  frappe.db.sql('''
+    # select name as sales_invoice, customer_address as address, customer, delivery_status from `tabSales Invoice` where territory = "{0}" and name in ({1})
+    # '''.format(territory,*sales_invoice),as_dict = 1)
     frappe.errprint(sales)
     return sales
+@frappe.whitelist()
+def update_invoice(invoice,fields):
+    fields = json.loads(fields)
+    frappe.errprint(fields['reason'])
+    frappe.db.set_value("Sales Invoice",invoice, 'delivery_status', fields['delivery_status'])
+    frappe.db.set_value("Sales Invoice",invoice, 'reason', fields['reason'])
+    frappe.db.commit()
+    return ""
+    
