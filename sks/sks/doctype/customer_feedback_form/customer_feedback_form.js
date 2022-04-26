d913084a1fdb4bb13051cdf5bbd0f05080ba8935
Shankarv19bcr <shankar@thirvusoft.in>
2022-04-26 17:01:01 +0530

    customer feedback form filter applied

--- sks/sks/doctype/customer_feedback_form/customer_feedback_form.js
+++ sks/sks/doctype/customer_feedback_form/customer_feedback_form.js
@@ -2,7 +2,20 @@
 // For license information, please see license.txt
 
 frappe.ui.form.on('Customer Feedback Form', {
-	// refresh: function(frm) {
-
-	// }
+	refresh: function(frm) {
+		let invoice_list = [];
+		cur_frm.fields_dict.invoice_no.$input.on("click", function() {
+			frappe.db.get_list("Customer Feedback Form",{fields:['invoice_no']}).then(function(data){
+				for(let i of data){invoice_list.push(i['invoice_no'])}
+				cur_frm.set_query('invoice_no',function(){
+					return {
+						filters:{
+							'name': ['not in',invoice_list]
+						}
+					}
+				})	
+			})
+			
+		});		
+	}
 });
