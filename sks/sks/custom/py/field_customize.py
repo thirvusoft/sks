a06b7e086fe8b7a0b67237a229dd876ccb27e27d
creative-paramu <pparameshwari@thirvusoft.in>
2022-06-21 20:11:05 +0530

    Purchase Order custom field in py

--- sks/sks/custom/py/field_customize.py
+++ sks/sks/custom/py/field_customize.py
@@ -19,7 +19,70 @@ def custom_fields():
         "Sales Invoice Item":[
              dict(fieldname='item_verified', label='Item Verified',
                 fieldtype='Check', insert_after='amount',read_only=1,in_list_view=1,columns=2),
-        ]
+        ],
+        "Purchase Order": [
+            dict(
+                label= "Place of Supply",
+                fieldname= "place_of_supply",
+                insert_after= "company_shipping_address",
+                fieldtype="Data",
+                allow_on_submit=0,
+                read_only= 1,
+                translatable= 1,
+                print_hide= 1,
+            ),
+            dict(
+                label= "Item Price Changed",
+                fieldname= "item_price_changed",
+                insert_after= "base_net_total",
+                fieldtype= "Text",
+                allow_on_submit=0,
+                read_only= 1,
+                hidden=1,
+                translatable= 1,
+            )
+            
+        ],
+        "Purchase Order Item": [
+            dict(
+                allow_on_submit= 1,
+                fetch_from= "item_code.gst_hsn_code",
+                fetch_if_empty= 1,
+                fieldname= "gst_hsn_code",
+                fieldtype= "Data",
+                insert_after= "description",
+                label= "HSN/SAC",
+                print_hide= 1,
+                translatable= 1,
+                unique= 0,
+            ),
+            dict(
+                fetch_from= "item_code.is_nil_exempt",
+                fieldname= "is_nil_exempt",
+                fieldtype= "Check",
+                insert_after= "gst_hsn_code",
+                label= "Is Nil Rated or Exempted",
+                print_hide= 1,
+                translatable= 1,
+            ),
+            dict(
+                fetch_from= "item_code.is_non_gst",
+                fieldname= "is_non_gst",
+                fieldtype= "Check",
+                insert_after= "is_nil_exempt",
+                label= "Is Non GST",
+                print_hide= 1,
+                translatable= 1,
+            ),
+            dict(
+                fieldname= "ts_mrp",
+                fieldtype= "Currency",
+                in_list_view= 1,
+                insert_after= "sec_break2",
+                label= "MRP",
+            ),
+            
+        ],
     }
     create_custom_fields(custom_fields)
     property_setter()
