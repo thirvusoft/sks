a7872e0d819c98a26c7db1b3396202971ef8f3d1
NavinThirvusoft <navin@thirvusoft.in>
2022-06-22 14:32:57 +0530

    Item Customization and Items to verify table Customization

--- sks/sks/utils/after_install.py
+++ sks/sks/utils/after_install.py
@@ -1,7 +1,9 @@
 from sks.sks.utils.buying.purchase_order.purchase_order_custom_fields import purchase_order_customization
 from sks.sks.utils.selling.delivery_note.delivery_note_custom_fields import delivery_note_customization
 from sks.sks.utils.selling.sales_order.sales_order_custom_fields import sales_order_customization
+from sks.sks.utils.stock.item.item_custom_fields import item_customization
 def after_install():
     purchase_order_customization()
     delivery_note_customization()
     sales_order_customization()
+    item_customization()
