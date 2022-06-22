593dca0e95e084daa283d799fd40599893906923
Navin S R <95605431+NavinThirvusoft@users.noreply.github.com>
2022-06-22 14:50:06 +0530

    Update after_install.py

--- sks/sks/utils/after_install.py
+++ sks/sks/utils/after_install.py
@@ -5,6 +5,7 @@ from sks.sks.utils.selling.delivery_note.delivery_note_custom_fields import deli
 from sks.sks.utils.selling.sales_order.sales_order_custom_fields import sales_order_customization
 from sks.sks.utils.stock.item.item_custom_fields import item_customization
 def after_install():
+    purchase_order_customization()
     delivery_note_customization()
     sales_order_customization()
     item_customization()
