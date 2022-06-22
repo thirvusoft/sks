12071543257996120bd3ae0f9a05c7733c9d9fa6
Gokulnath17 <gokulnath@thirvusoft.in>
2022-06-22 12:46:29 +0530

    feat:delivery note field customization

--- sks/sks/utils/after_install.py
+++ sks/sks/utils/after_install.py
@@ -1,3 +1,5 @@
 from sks.sks.utils.buying.purchase_order.purchase_order_custom_fields import purchase_order_customization
+from sks.sks.utils.selling.delivery_note.delivery_note_custom_fields import  delivery_note_customization
 def after_install():
-          purchase_order_customization()
+    purchase_order_customization()
+    delivery_note_customization()
---
+++ sks/sks/utils/selling/delivery_note/delivery_note_item/delivery_note_item_custom_fields.py
@@ -0,0 +1,18 @@
+import frappe
+from frappe.custom.doctype.custom_field.custom_field import create_custom_fields
+from frappe.custom.doctype.property_setter.property_setter import make_property_setter
+
+def delivery_note_item_custom_fields():
+    custom_fields={
+        "Delivery Note Item":[
+            dict(fieldname='item_verified', label='Item Verified',
+                fieldtype='Check', insert_after='column_break_54',options="Barcode",in_list_view=1,columns=2,read_only=1),
+        ]
+    }
+    create_custom_fields(custom_fields)
+
+def delivery_note_item_property_setter():
+    make_property_setter("Delivery Note Item", "is_nil_exempt", "hidden", "1", "Check")
+    make_property_setter("Delivery Note Item", "is_non_gst", "hidden", "1", "Check")
+    make_property_setter("Delivery Note Item", "section_break_72", "hidden", "1", "Section Break")
+    make_property_setter("Delivery Note Item", "allow_zero_valuation_rate", "hidden", "1", "Check")
