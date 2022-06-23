30be21521d28fabfc348c070ff4526ebc744e673
sibikumarkuppusamy <sibi.kumar@thirvusoft.in>
2022-06-23 10:15:21 +0530

    customization in contact

---
+++ sks/sks/utils/crm/customer/contact/contact_custom_fields.py
@@ -0,0 +1,14 @@
+from frappe.custom.doctype.custom_field.custom_field import create_custom_fields
+from frappe.custom.doctype.property_setter.property_setter import make_property_setter
+def contact_customization():
+    contact_custom_field()
+    contact_property_setter()
+     
+def contact_custom_field():
+    pass
+
+def contact_property_setter(): 
+    make_property_setter("Contact", "more_info", "hidden", "1", "Check")
+    make_property_setter("Contact", "department", "hidden", "1", "Check")
+    make_property_setter("Contact", "unsubscribed", "hidden", "1", "Check")
+   
