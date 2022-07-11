4aee9498be898e17fb50da26801e4f18244cbdc5
Navin <navin@thirvusoft.in>
2022-07-11 12:41:51 +0530

    Selling module dashboards

---
+++ sks/sks/report/thirvu_userwise___selling_module_1/thirvu_userwise___selling_module_1.js
@@ -0,0 +1,31 @@
+// Copyright (c) 2022, Thirvusoft and contributors
+// For license information, please see license.txt
+/* eslint-disable */
+
+frappe.query_reports["Thirvu Userwise - Selling Module_1"] = {
+	"filters": [
+		{
+			"fieldname":"from_date",
+			"lable": __("From Date"),
+			"fieldtype": "Date",
+			"default":frappe.datetime.get_today(),
+            "width": "80"
+		},
+		{
+            "fieldname":"to_date",
+            "label": __("To Date"),
+            "fieldtype": "Date",
+            "default": frappe.datetime.get_today()
+        },
+		{
+			"fieldname":"employee",
+            "label": __("Employee Name"),
+            "fieldtype": "Data",
+			"read_only":1,
+		}
+	],
+	onload: function(report){
+		console.log(frappe.session.user)
+		frappe.query_report.set_filter_value("employee",frappe.session.user);
+	}
+};
