3fedcaf87094c5d57caf125245be3083cc1dd11f
Nandhinidevi123 <nandhini.devi@thirvusoft.in>
2022-07-11 18:17:23 +0530

    chores:small fixes in sales invoice

--- sks/sks/custom/js/sales_invoice.js
+++ sks/sks/custom/js/sales_invoice.js
@@ -272,5 +272,15 @@ frappe.ui.form.on("Sales Invoice",{
 			}
 		})
 	}
+	
 })
 	
+frappe.ui.form.on("Sales Invoice",{
+	onload:function(frm,cdt,cdn){
+		if(cur_frm.doc.items[0].delivery_note){
+			console.log("hhjdjdj")
+			frm.set_df_property('update_stock', 'hidden', 1);
+		
+	}},
+	
+ })
