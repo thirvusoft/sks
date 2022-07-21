e8db98f948dea0aa14ef262a122c9e9e7892858e
Gokulnath17 <gokulnath@thirvusoft.in>
2022-07-21 10:12:58 +0530

    chores:fixes

--- sks/sks/custom/py/batch.py
+++ sks/sks/custom/py/batch.py
@@ -1,6 +1,6 @@
 import frappe
 def item_price_creator(doc,action):
-	# if doc.reference_doctype=="Purchase Receipt":
+	if doc.reference_doctype=="Purchase Receipt":
 		ts_new_item_price=frappe.get_doc({
 			"doctype":"Item Price",
 			"item_code":doc.item,
