9aedf75a4f99c6c3c7ebea0770d4ca13cba5a160
Nandhinidevi123 <nandhini.devi@thirvusoft.in>
2022-07-22 10:51:05 +0530

    employee bonus tool

--- sks/sks/doctype/employee_bonus_tool/employee_bonus_tool.js
+++ sks/sks/doctype/employee_bonus_tool/employee_bonus_tool.js
@@ -3,13 +3,10 @@
 	
 frappe.ui.form.on("Employee Bonus Tool",{
 	department:function(frm,cdt,cdn){
-		console.log("dggdhhdh")
 		var bonus=locals[cdt][cdn]
 		var from_date=frm.doc.from_date
 		var to_date = frm.doc.to_date
 		var bonus1=bonus.department
-		console.log(bonus1)
-
 		if(!from_date || !to_date){
 			frappe.throw("Please Select From Date And To Date")
 		}
@@ -19,27 +16,28 @@ frappe.ui.form.on("Employee Bonus Tool",{
 				method:"sks.sks.doctype.employee_bonus_tool.employee_bonus_tool.employee_finder",
 				args:{bonus1,from_date,to_date},
 				callback(r){
-					console.log(r)
+					var total =0
 					frm.trigger("from_date")
 					frm.trigger("to_date")
 					frm.clear_table("employee_bonus_details");
 					for(var i=0;i<r.message[0].length;i++){
 						var child = cur_frm.add_child("employee_bonus_details");
+				        total += r.message[1][i]
 						frappe.model.set_value(child.doctype, child.name, "employee", r.message[0][i]["name"])
 						frappe.model.set_value(child.doctype, child.name, "employee_name", r.message[0][i]["employee_name"])
 						frappe.model.set_value(child.doctype, child.name, "department", bonus1)
 						frappe.model.set_value(child.doctype, child.name, "current_bonus", r.message[1][i])
 					}
+					cur_frm.set_value("total_bonus_amount",total)
 					cur_frm.refresh_field("employee_bonus_details")
 				}
+				
 			})
 		
-
-
-	}
-			
-			
+		
+		}
 	},
+	
 	on_submit:function(frm,cdt,cdn){
 		var bonus=locals[cdt][cdn]
 		for(var i=0;i<bonus.employee_bonus_details.length;i++){
@@ -52,13 +50,7 @@ frappe.ui.form.on("Employee Bonus Tool",{
 			})
 		}
 	},
-	before_save:function(frm, cdt, cdn) {
-		var table = frm.doc.employee_bonus_details;
-		var total = 0;
-		for(var i in table) {
-			total = total + table[i].current_bonus;
-		 }
-		 frm.set_value("total_bonus_amount",total);
-		}
+	
+	
 });
 	 
--- sks/sks/doctype/employee_bonus_tool/employee_bonus_tool.py
+++ sks/sks/doctype/employee_bonus_tool/employee_bonus_tool.py
@@ -12,28 +12,29 @@ def employee_finder(bonus1,from_date,to_date):
     start = datetime.strptime(from_date, '%Y-%m-%d')
     end = datetime.strptime(to_date, '%Y-%m-%d')
     total_month = (end.year - start.year) * 12 + (end.month - start.month)
-    emp_list=frappe.db.get_all("Employee",filters={"department":bonus1},fields=["name", "employee_name"])
-    for name in emp_list:
-        
-        attendance_status = frappe.db.sql("""
-            SELECT count(att.name)
-            FROM `tabAttendance` as att
-            WHERE  att.employee = '{0}' and att.attendance_date between '{1}' and '{2}' and att.status = 'Present'
-            """.format(name['name'],from_date,to_date),as_list=1)[0][0]
+    if(total_month !=0):
+        emp_list=frappe.db.get_all("Employee",filters={"department":bonus1},fields=["name", "employee_name"])
+        for name in emp_list:
+            
+            attendance_status = frappe.db.sql("""
+                SELECT count(att.name)
+                FROM `tabAttendance` as att
+                WHERE  att.employee = '{0}' and att.attendance_date between '{1}' and '{2}' and att.status = 'Present'
+                """.format(name['name'],from_date,to_date),as_list=1)[0][0]
+            emp_base_amount=frappe.db.sql("""select ssa.base
+                    FROM `tabSalary Structure Assignment` as ssa
+                    WHERE ssa.employee = '{0}' and ssa.from_date <='{1}'
+                    ORDER BY ssa.from_date DESC LIMIT 1 """.format(name['name'],to_date),as_list=1)
+            if emp_base_amount:
+                calc = (float(attendance_status) * float(emp_base_amount[0][0])/total_month)
 
-        print(attendance_status)
-        emp_base_amount=frappe.db.sql("""select ssa.base
-                FROM `tabSalary Structure Assignment` as ssa
-                WHERE ssa.employee = '{0}' and ssa.from_date <='{1}'
-                ORDER BY ssa.from_date DESC LIMIT 1 """.format(name['name'],to_date),as_list=1)
-        print(emp_base_amount)
-        if emp_base_amount:
-            calc = (float(attendance_status) * float(emp_base_amount[0][0])/total_month)
-
-            amount.append(calc)
-        employee_names.append(name)
-        
-    return employee_names,amount
+                amount.append(calc)
+            employee_names.append(name)
+            
+        return employee_names,amount
+    else:
+        frappe.throw("Please Select Date Grater Than One Month")
+    
 
 @frappe.whitelist()
 def create_bonus(name,amount,date,doc):
