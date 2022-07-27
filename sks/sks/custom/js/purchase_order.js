15a3b37b44801c8305f571cc7738a585d7e90465
NandhiniDevi <95607404+Nandhinidevi123@users.noreply.github.com>
2022-07-27 14:56:09 +0530

    Update purchase_order.js

--- sks/sks/custom/js/purchase_order.js
+++ sks/sks/custom/js/purchase_order.js
@@ -174,7 +174,7 @@ frappe.ui.form.on("Purchase Order Item",{
 								warehouse=cur_frm.doc.ts_warehouse
 							}
 							else{
-								frappe.show_alert({ message: __('Please Select Warehouse for Item'+item_code), indicator: 'red' });
+								frappe.show_alert({ message: __('Please Select Warehouse for Item '+item_code), indicator: 'red' });
 							}
                         }
                     })
