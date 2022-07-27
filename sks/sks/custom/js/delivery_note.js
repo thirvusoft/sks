e2f09f167ef04ca7caf1332b7bbbe52b022324a1
NandhiniDevi <95607404+Nandhinidevi123@users.noreply.github.com>
2022-07-27 19:07:49 +0530

    Update delivery_note.js

--- sks/sks/custom/js/delivery_note.js
+++ sks/sks/custom/js/delivery_note.js
@@ -175,7 +175,7 @@ frappe.ui.form.on("Delivery Note Item",{
 							if(r.message){
 								frappe.model.set_value(data.doctype, data.name, "warehouse", r.message)
 								frappe.model.set_value(data.doctype, data.name, "ts_warehouse", r.message)
-								warehouse=cur_frm.doc.ts_warehouse
+								
 							}
 							else{
 								frappe.show_alert({ message: __('Please Select Warehouse for Item '+item_code), indicator: 'red' });
