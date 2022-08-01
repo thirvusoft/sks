1c88c4f6d2c45d3571f44d8e13858986fd24190d
NaviN <navin@thirvusoft.in>
2022-08-01 14:40:51 +0530

    feat:delivery trip alerts and bug fixes

--- sks/sks/custom/js/customer.js
+++ sks/sks/custom/js/customer.js
@@ -1,6 +1,5 @@
 frappe.ui.form.on("Customer",{
     onload:function(frm){
-        console.log(frappe.user_roles)
         if (in_list(frappe.user_roles, "Accounts Manager")) {
             frm.set_df_property("is_credit_customer", "hidden",0);
         }
