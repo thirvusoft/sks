4c199b6e5d0f8ca597757be26f8523e472f63f79
ChandanaSPthirvusoft <chandana@thirvusoft.in>
2022-08-02 18:48:38 +0530

    rewrite file

--- sks/sks/custom/py/employee.py
+++ sks/sks/custom/py/employee.py
@@ -5,51 +5,56 @@ from datetime import timedelta,datetime
 from sks.sks.custom.py.default import validate_contact
 from datetime import datetime 
 from frappe.utils import getdate , get_date_str
+from dateutil.relativedelta import relativedelta
 
 
 def validate_phone(doc,action):
           validate_contact(doc.cell_number)
           validate_contact(doc.emergency_phone_number)
+          property_deduction(doc)
 
-@frappe.whitelist()
 def property_deduction(doc):
-    doc = json.loads(doc)
-    amount = 0
-    date_format = '%Y-%m-%d'
-    if not frappe.db.exists("Additional Salary", {"ref_docname": doc['name']}):
-        additional_salary = frappe.new_doc('Additional Salary')
-        additional_salary.ref_docname = doc['name']
-        additional_salary.ref_doctype = doc['doctype']
-        additional_salary.payroll_date = datetime.strptime(doc['relieving_date'],date_format) - timedelta(1)
-        additional_salary.employee = doc['name']
-        additional_salary.salary_component = frappe.db.get_value("Thirvu Penalty Reason",self.reason,'salary_component')
-        for data in doc['ts_property_details']:
-            amount += data['amount'] * data['count']
-        additional_salary.amount = amount
-        additional_salary.overwrite_salary_structure_amount = 0
-        additional_salary.save()
-        additional_salary.submit()
-        frappe.db.commit()
+    if doc.relieving_date:
+        one_yr_date = getdate(doc.date_of_joining) + relativedelta(years=1)
+        if getdate(doc.relieving_date) <= one_yr_date:
+            amount = 0
+            date_format = '%Y-%m-%d'
+            if not frappe.db.exists("Additional Salary", {"ref_docname": doc.name}):
+                additional_salary = frappe.new_doc('Additional Salary')
+                additional_salary.ref_docname = doc.name
+                additional_salary.ref_doctype = doc.doctype
+                additional_salary.payroll_date = datetime.strptime(doc.relieving_date,date_format) - timedelta(1)
+                additional_salary.employee = doc.name
+                if frappe.db.get_single_value("Thirvu HR Settings", "property_reduction_component"):
+                    additional_salary.salary_component = frappe.db.get_single_value("Thirvu HR Settings", "property_reduction_component")
+                for data in doc.ts_property_details:
+                    amount += data.amount * data.count
+                additional_salary.amount = amount
+                additional_salary.overwrite_salary_structure_amount = 0
+                additional_salary.save()
+                additional_salary.submit()
+                frappe.db.commit()
 
-    else:
-        old = frappe.get_doc('Additional Salary',{'ref_docname':doc['name']})
-        old.cancel()
-        additional_salary = frappe.copy_doc(old) 
-        additional_salary.amended_from = old.name 
-        additional_salary.status = "Draft" 
-        additional_salary.insert()
-        additional_salary.ref_docname = doc['name']
-        additional_salary.ref_doctype = doc['doctype']
-        additional_salary.payroll_date = datetime.strptime(doc['relieving_date'],date_format) - timedelta(1)
-        additional_salary.employee = doc['name']
-        additional_salary.salary_component = frappe.db.get_value("Thirvu Penalty Reason",self.reason,'salary_component')
-        for data in doc['ts_property_details']:
-            amount += data['amount'] * data['count']
-        additional_salary.amount = amount
-        additional_salary.overwrite_salary_structure_amount = 0
-        additional_salary.save()
-        additional_salary.submit()
-        frappe.db.commit()
+            else:
+                old = frappe.get_doc('Additional Salary',{'ref_docname':doc.name})
+                old.cancel()
+                additional_salary = frappe.copy_doc(old) 
+                additional_salary.amended_from = old.name 
+                additional_salary.status = "Draft" 
+                additional_salary.insert()
+                additional_salary.ref_docname = doc.name
+                additional_salary.ref_doctype = doc.doctype
+                additional_salary.payroll_date = datetime.strptime(doc.relieving_date,date_format) - timedelta(1)
+                additional_salary.employee = doc.name
+                if frappe.db.get_single_value("Thirvu HR Settings", "property_reduction_component"):
+                    additional_salary.salary_component = frappe.db.get_single_value("Thirvu HR Settings", "property_reduction_component")               
+                for data in doc.ts_property_details:
+                    amount += data.amount * data.count
+                additional_salary.amount = amount
+                additional_salary.overwrite_salary_structure_amount = 0
+                additional_salary.save()
+                additional_salary.submit()
+                frappe.db.commit()
         
 @frappe.whitelist()
 def age_calculation(date_of_birth):
