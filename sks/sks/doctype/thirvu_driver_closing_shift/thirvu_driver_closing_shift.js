855e0bd16d37658f1dd793ca920c82148b2245f2
ChandanaSPthirvusoft <chandana@thirvusoft.in>
2022-08-03 10:01:09 +0530

    chores:dialog if condition in driver closing shift

--- sks/sks/doctype/thirvu_driver_closing_shift/thirvu_driver_closing_shift.js
+++ sks/sks/doctype/thirvu_driver_closing_shift/thirvu_driver_closing_shift.js
@@ -9,70 +9,72 @@ frappe.ui.form.on('Thirvu Driver Closing Shift', {
 				doc: frm.doc,
 			},
 			callback: function(r) {
-				var d = new frappe.ui.Dialog({
-					size: "large",
-					title:"Choose Account Head",
-					fields:[
-					{fieldname:'account_details', fieldtype:'Table',cannot_add_rows: 1,in_place_edit: true, fields:[
-					{
-						label: 'Mode of Payment',
-						fieldname: 'mode_of_payment',
-						fieldtype: 'Read Only',
-						in_list_view:1,
-						read_only:1,
-						columns:2
-					},
-					{
-						label: 'Amount',
-						fieldname: 'amount',
-						fieldtype: 'Read Only',
-						in_list_view:1,
-						read_only:1,
-						columns:2
-					},
-					{
-						label: 'Debit Account Head',
-						fieldname: 'debit_account_head',
-						fieldtype: 'Read Only',
-						in_list_view:1,
-						read_only:1,
-						columns:2,
-					},
-					{
-						label: 'Credit Account Head',
-						fieldname: 'credit_account_head',
-						fieldtype: 'Link',
-						options:"Account",	
-						in_list_view:1,
-						columns:2
-					},
-					],
-					  data:r.message},
-					],
-					primary_action : function(data){
-						for (let value in data.account_details){
-							if(!data.account_details[value].credit_account_head){
-								frappe.throw({message:__("Please select Credit Account for "+ data.account_details[value].mode_of_payment ), title: __("Mandatory")});
-							}
-						}
-						frappe.call({
-							method: "posawesome.posawesome.api.posapp.create_journal_entry",
-							args: {
-								data: data,
-								doc:frm.doc
-							},
-							callback:function(data){
-								if(data.message == 1){
-									frappe.show_alert({ message: __('Journal Entry Created Successfully.'), indicator: 'green' });
-
+				if(r.message.length != 0){
+					var d = new frappe.ui.Dialog({
+						size: "large",
+						title:"Choose Account Head",
+						fields:[
+						{fieldname:'account_details', fieldtype:'Table',cannot_add_rows: 1,in_place_edit: true, fields:[
+						{
+							label: 'Mode of Payment',
+							fieldname: 'mode_of_payment',
+							fieldtype: 'Read Only',
+							in_list_view:1,
+							read_only:1,
+							columns:2
+						},
+						{
+							label: 'Amount',
+							fieldname: 'amount',
+							fieldtype: 'Read Only',
+							in_list_view:1,
+							read_only:1,
+							columns:2
+						},
+						{
+							label: 'Debit Account Head',
+							fieldname: 'debit_account_head',
+							fieldtype: 'Read Only',
+							in_list_view:1,
+							read_only:1,
+							columns:2,
+						},
+						{
+							label: 'Credit Account Head',
+							fieldname: 'credit_account_head',
+							fieldtype: 'Link',
+							options:"Account",	
+							in_list_view:1,
+							columns:2
+						},
+						],
+						  data:r.message},
+						],
+						primary_action : function(data){
+							for (let value in data.account_details){
+								if(!data.account_details[value].credit_account_head){
+									frappe.throw({message:__("Please select Credit Account for "+ data.account_details[value].mode_of_payment ), title: __("Mandatory")});
 								}
 							}
-						})
-					d.hide();
-					},
-					primary_action_label: __('Create Journal Entry')
-				});
-		  d.show();
+							frappe.call({
+								method: "sks.sks.doctype.thirvu_driver_closing_shift.thirvu_driver_closing_shift.create_journal_entry",
+								args: {
+									data: data,
+									doc:frm.doc
+								},
+								callback:function(data){
+									if(data.message == 1){
+										frappe.show_alert({ message: __('Journal Entry Created Successfully.'), indicator: 'green' });
+	
+									}
+								}
+							})
+						d.hide();
+						},
+						primary_action_label: __('Create Journal Entry')
+					});
+			  d.show();
+				}
 		}
 	});
 	},
--- sks/sks/doctype/thirvu_driver_closing_shift/thirvu_driver_closing_shift.py
+++ sks/sks/doctype/thirvu_driver_closing_shift/thirvu_driver_closing_shift.py
@@ -2,6 +2,7 @@
 # For license information, please see license.txt
 
 import frappe,json
+from frappe import _
 from frappe.model.document import Document
 
 class ThirvuDriverClosingShift(Document):
@@ -15,10 +16,31 @@ def get_payment_details(doc):
 			row = frappe._dict()
 			account = frappe.get_doc('Mode of Payment',data['mode_of_payment'])
 			if data['closing_amount']>0:
-				print('ji')
 				row.update({'mode_of_payment':data['mode_of_payment'],'amount':data['closing_amount'],'debit_account_head':account.accounts[0].default_account})
 				table.append(row)
 	except:
 		pass
-	print('hi')
 	return table
+
+
+@frappe.whitelist()
+def create_journal_entry(data,doc):
+	data = json.loads(data)
+	doc = json.loads(doc)
+
+	new_jv_doc=frappe.new_doc('Journal Entry')
+	new_jv_doc.voucher_type='Journal Entry'
+	new_jv_doc.posting_date=doc['posting_date']
+	new_jv_doc.company = doc['company']
+	new_jv_doc.user_remark = _("POS Closing Shift for {0} to {1}").format(
+				doc['period_start_date'], doc['period_end_date']
+			)
+
+	for value in range(0,len(data['account_details']),1):
+		new_jv_doc.append('accounts',{'account':data['account_details'][value]['debit_account_head'],'credit_in_account_currency':data['account_details'][value]['amount']})
+		new_jv_doc.append('accounts',{'account':data['account_details'][value]['credit_account_head'],'debit_in_account_currency':data['account_details'][value]['amount']})
+		
+
+	new_jv_doc.insert()
+	new_jv_doc.submit()
+	return 1
